459143ce8c96a0eab49ee2f2d4bb100e169e7908
MAXANGE2B <maxange2b@gmail.com>
2022-12-17 20:40:19 +0100

    update

---
+++ Assets/OwO_Butons_Script.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+using Newtonsoft.Json;
+using Rnd = UnityEngine.Random;
+
+public class OwO_Butons_Script : MonoBehaviour 
+{
+
+    public class ModSettingsJSON
+    {
+        public float TimeLimit;
+        public int _stageCount;
+    }
+
+
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+    public KMBombModule Module;
+    public KMColorblindMode Colorblind;
+    public KMModSettings modSettings;
+
+    public KMSelectable[] Buttons;
+    public TextMesh[] ButtonsText;
+    public MeshRenderer[] ButtonsLED;
+    public MeshRenderer LED;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
